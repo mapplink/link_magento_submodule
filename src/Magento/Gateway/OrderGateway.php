39206885fdfa3f28c02ac486359610761c9618ec
gelysis <andreas@gelysis.net>
2014-07-11 17:56:30 +1200

    Cleanup

--- src/Magento/Gateway/OrderGateway.php
+++ src/Magento/Gateway/OrderGateway.php
@@ -123,7 +123,8 @@ class OrderGateway extends AbstractGateway
 
                 $data = array(
                     'customer_email'=> array_key_exists('customer_email', $order) ? $order['customer_email'] : null,
-                    'customer_name'=>(array_key_exists('customer_firstname', $order) ? $order['customer_firstname'].' ' : '') . (array_key_exists('customer_lastname', $order) ? $order['customer_lastname'] : ''),
+                    'customer_name'=>(array_key_exists('customer_firstname', $order) ? $order['customer_firstname'].' ' : '')
+                        .(array_key_exists('customer_lastname', $order) ? $order['customer_lastname'] : ''),
                     'status'=>$order['status'],
                     'placed_at'=>$order['created_at'],
                     'grand_total'=>$order['grand_total'],
