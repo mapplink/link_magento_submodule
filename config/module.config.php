61a56a16ca010687da9f6d7296a5bc5b72ae3944
gelysis <andreas@gelysis.net>
2014-08-11 11:17:44 +1200

    Cleanup

--- config/module.config.php
+++ config/module.config.php
@@ -47,11 +47,11 @@ return array (
                 'db_username'=>array('label'=>'Database Username','type'=>'Text', 'required'=>FALSE),
                 'db_password'=>array('label'=>'Database Password','type'=>'Text', 'required'=>FALSE),
 
-
                 'load_full_product'=>array('label'=>'Load full product data', 'type'=>'Checkbox', 'default'=>FALSE),
                 'load_stock'=>array('label'=>'Load stock data (SLOW)', 'type'=>'Checkbox', 'default'=>FALSE),
                 'load_full_customer'=>array('label'=>'Load full customer data', 'type'=>'Checkbox', 'default'=>FALSE),
                 'load_full_order'=>array('label'=>'Load full order data', 'type'=>'Checkbox', 'default'=>FALSE),
+
                 'product_attributes'=>array(
                     'label'=>'Extra product attributes to load',
                     'type'=>'Text',
